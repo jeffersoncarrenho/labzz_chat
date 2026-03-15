8c05f124c1f83f0a91dbd476f34976280ab09401
jeffersoncarrenho <jefferson.carrenho@gmail.com>
2026-03-15 13:59:48 -0300

    feat: configure realtime broadcasting using Laravel Reverb

---
+++ backend/app/Events/MessageSent.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Message;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
+
+class MessageSent implements ShouldBroadcast
+{
+    use Dispatchable, SerializesModels;
+
+    public $message;
+
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+    }
+
+    public function broadcastOn()
+    {
+        return new Channel('conversation.' . $this->message->conversation_id);
+    }
+
+    public function broadcastWith()
+    {
+        return [
+            'id' => $this->message->id,
+            'conversation_id' => $this->message->conversation_id,
+            'user_id' => $this->message->user_id,
+            'content' => $this->message->content,
+            'created_at' => $this->message->created_at
+        ];
+    }
+}
--- backend/app/Services/MessageService.php
+++ backend/app/Services/MessageService.php
@@ -3,15 +3,20 @@
 namespace App\Services;
 
 use App\Models\Message;
+use App\Events\MessageSent;
 
 class MessageService
 {
-    public function createMessage(array $data)
+   public function createMessage(array $data)
     {
-        return Message::create([
+        $message = Message::create([
             'conversation_id' => $data['conversation_id'],
             'user_id' => $data['user_id'],
             'content' => $data['content']
         ]);
+
+        event(new MessageSent($message));
+
+        return $message;
     }
 }
